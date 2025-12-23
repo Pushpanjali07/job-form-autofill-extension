47aad437a894ef21c6d99e26586e909392d042e2
Pushpanjali <125995319+Pushpanjali07@users.noreply.github.com>
2025-12-23 12:19:39 +0530

    Add event listener for autofill button

--- popup.js
+++ popup.js
@@ -1,7 +1,13 @@
+// Get the Autofill button and listen for click
 document.getElementById("autofillBtn").addEventListener("click", () => {
+
+  // Get the currently active tab in the current window
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+
+    // Send a message to the content script in that tab
     chrome.tabs.sendMessage(tabs[0].id, {
       action: "AUTOFILL_FORM"
     });
   });
 });
+
